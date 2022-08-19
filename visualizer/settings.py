193cebc958ed98b4e57a2b0ff88733197b5bc696
David Svaty <davo444ms@gmail.com>
2022-08-19 12:52:49 +0200

    New vertical timeline page added

--- visualizer/settings.py
+++ visualizer/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'visualizer.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,6 +119,11 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'timeline/static'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
