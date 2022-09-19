7132146f2ad1fc46bf93113b25c7510f38736d93
Lukáš Kaprál <80596523+FITlifeSADGE@users.noreply.github.com>
2022-09-19 16:16:25 +0200

    Added DateTimeForm

--- timeline/forms.py
+++ timeline/forms.py
@@ -1,5 +1,11 @@
+from sqlite3 import Date
 from django import forms
+from .widgets import DatePickerInput, TimePickerInput
 
 class UploadFileFom(forms.Form):
     title = forms.CharField(max_length=100)
     file = forms.FileField()
+
+class DateTimeForm(forms.Form):
+    date = forms.DateField(widget=DatePickerInput)
+    time = forms.TimeField(widget=TimePickerInput)
