113409a7ce50c08fe8396cc632fb7cfbcbc876d9
David Svaty <davo444ms@gmail.com>
2022-09-27 11:50:43 +0200

    Default values on date filter

--- timeline/forms.py
+++ timeline/forms.py
@@ -1,11 +1,22 @@
 from sqlite3 import Date
 from django import forms
 from .widgets import DatePickerInput, TimePickerInput
+from .models import Log
+import datetime
 
 class UploadFileFom(forms.Form):
     title = forms.CharField(max_length=100)
     file = forms.FileField()
 
 class DateTimeForm(forms.Form):
-    date = forms.DateField(widget=DatePickerInput)
+
+    date = forms.DateField(widget=DatePickerInput, initial=datetime.date.today)
     time = forms.TimeField(widget=TimePickerInput)
+
+    # Add default date value from log 
+    def_datetime = Log.objects.all().values('dateTime')
+    def_datetime = def_datetime.first()['dateTime']
+    print(def_datetime)
+    if def_datetime:
+        def_date = datetime.datetime.strftime(def_datetime, "%Y-%m-%d")
+        date = forms.DateField(widget=DatePickerInput, initial=def_date)
